9b721330e83107cb5052b95294b780b2e2d6fe59
edumur <etienne.dumur@gmail.com>
2016-05-26 12:32:37 -0500

    Correct merge conflict

--- LJPA.py
+++ LJPA.py
@@ -255,52 +255,6 @@ class LJPA(JPA, Find):
 
 
 
-<<<<<<< HEAD
-=======
-    def find_reflection_fwhm(self):
-        """
-        Return the half width at half maximum in Hz of the power reflection.
-        Numerically estimated.
-        """
-
-        def func(f,half_max):
-            return abs(-abs(self.reflection(f))**2. + half_max)
-
-        f0 = self.find_resonance_frequency()
-        half_max = (  abs(self.reflection(f0))**2.\
-                    + abs(self.reflection(f0+100e9))**2.)/2.
-        df = minimize_scalar(func, bounds=(1., 100e9),
-                                   method='bounded',
-                                   args=[half_max]).x[0]
-
-        return abs(f0 - df)*2.
-
-
-
-    def find_angular_resonance_frequency(self):
-        """
-        Return the angular resonance frequency in rad.Hz of the power reflection.
-        Numerically estimated.
-        """
-
-        return self.find_resonance_frequency()*2.*np.pi
-
-
-
-    def find_resonance_frequency(self):
-        """
-        Return the resonance frequency in Hz of the power reflection.
-        Numerically estimated.
-        """
-
-        def func(f):
-            return -abs(self.reflection(f))**2.
-
-        return minimize_scalar(func, bounds=(1., 100e9), method='bounded').x[0]
-
-
-
->>>>>>> 2714ae183b0e7c1a2be69697ff4ea281a1e53650
     def optimized_squid_inductance_imag(self, R0=50.):
         """
         Return the imaginary part of the SQUID inductance for which the
